3ff1365781d11471d8e3b3d5e47b568536c3e41f
Felipe Bonifácio <felipebonifacio@gmail.com>
2025-11-23 21:08:11 -0300

    Update the new HP bar system
    - HP bar with new animation

--- Scripts/hud.gd
+++ Scripts/hud.gd
@@ -1,30 +1,54 @@
 extends CanvasLayer
 
-@onready var bar: ProgressBar = $HealthBarContainer/ProgressBar
+@onready var hp_bar: ProgressBar = $HealthBar
 
+var displayed_value := 0.0
 
-func _ready() -> void:
-	#conecta automaticamente ao player quando ele aparece na cena
-	var player = get_tree().get_first_node_in_group("player")
-	if player:
-		player.health_changed.connect(_on_player_health_changed)
+func update_health(current: int, max_health: int) -> void:
+	if hp_bar == null:
+		return
 		
-func _on_player_health_changed(current, max_health):
-	bar.max_value = max_health
-	bar.value = current
+	hp_bar.max_value = max_health
+	hp_bar.value = current
 	
-	$HealthBarContainer/ProgressBar/Label.text = str(current, " / ", max_health)
+	#Tween suave para o valor exibido
+	var tween := create_tween()
+	tween.tween_property(self, "displayed_value", current, 0.25)\
+	.set_trans(Tween.TRANS_SINE)\
+	.set_ease(Tween.EASE_OUT)
+	tween.connect("finished", Callable(self, "_apply_bar_value"))
 	
-	_uptade_bar_color(current, max_health)
+	_update_color(current, max_health)
 	
-
-func _uptade_bar_color(current, max_health):
-	var ratio = float(current) / float(max_health)
-	var fill = bar.get("theme_override_styles/fill")
+func _apply_bar_value() -> void:
+	#aplica o valor final na barra após a animação
+	hp_bar.value = displayed_value
+	
+func _update_color(current: int, max_health: int) -> void:
+	var ratio := float(current) / float(max_health)
+	var fill_style: StyleBoxFlat = hp_bar.get_theme_stylebox("fill")
+	
+	var target_color := Color.GREEN
 	
 	if ratio > 0.6:
-		fill.bg_color = Color(0 ,1, 0) #verde
+		target_color = Color(0,1,0) #Verde
 	elif ratio > 0.3:
-		fill.bg_color = Color(1, 0.8, 0) #amarelo
+		target_color = Color(1,0.8,0) #Amarelo
 	else:
-		fill.bg_color = Color(1, 0, 0) #vermelho
+		target_color = Color(1,0,0) #Vermelho
+		
+	#Tween suave para a cor
+	var tween := create_tween()
+	tween.tween_property(fill_style, "bg_color", target_color, 0.25)
+	
+	
+func flash_damage() -> void:
+	var tween := create_tween()
+	hp_bar.modulate = Color(1,0.6,0.6)
+	tween.tween_property(hp_bar, "modulate", Color.WHITE, 0.3)\
+	.set_trans(Tween.TRANS_SINE)\
+	.set_ease(tween.EASE_OUT)
+	
+	
+	
+	
